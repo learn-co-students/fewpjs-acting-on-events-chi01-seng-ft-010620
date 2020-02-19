8f7743a897c48795747e5f65fd9351813b2d1b2d
learn <github email address>
2020-02-18 16:25:04 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+let dodger = document.getElementById("dodger")
+dodger
+
+document.addEventListener("keydown", function(e){
+    console.log(e.key);
+});
+
+document.addEventListener("keydown", function(e){
+    if (e.key === "ArrowLeft"){
+        let leftNumbers = dodger.style.left.replace("px", "");
+        let left = parseInt(leftNumbers, 10);
+        dodger.style.left = `${left - 1}px`;
+    }
+});
+
+document.addEventListener("keydown", function(e){
+    if (e.key === "ArrowRight"){
+        let leftNumbers = dodger.style.left.replace("px", ""); 
+        let left = parseInt(leftNumbers, 10); 
+        dodger.style.left = `${left + 1}px`;
+    }
+});
+
